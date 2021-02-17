9b5052c0a26351bccb0358017efd4d42fb2ae113
MohammadReza <md.mohammadreza+git@gmail.com>
2021-02-17 14:55:05 +0330

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        my_background: '#f7f9fd'
+      }
+    },
   },
   variants: {
     extend: {},
